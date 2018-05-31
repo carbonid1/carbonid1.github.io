c4a76c2a02d14670e9b7eb56045bb17982e5bb6e
Сoriander <36999873+carbonid1@users.noreply.github.com>
2018-05-31 17:16:58 +0300

    tictac 0.3

--- TicTacToeGame/js/main.js
+++ TicTacToeGame/js/main.js
@@ -1,27 +1,34 @@
 const body = document.querySelector("Body");
-const moduleContent = document.querySelector(".module-content");
+const wrapper = document.querySelector(".wrapper");
+const modalWindow = document.querySelector(".modal-window");
+const modalContent = document.querySelector(".modal-content");
+const fieldContainer = document.querySelector(".field-container");
 
-//rdy function to substitute jQuery
+// ready function to substitute jQuery
 function ready(fn) {
   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
+
+  return fn;
 }
 
 // OLOO
 let Widget = {
-  init: function(width=50, height=50) {
+  init: function(widgetClass, width=50, height=50) {
     this.width = width;
-    this.heigth = height;
+    this.height = height;
     this.elem = null;
+    this.class = widgetClass;
   },
 
   insert: function(where) {
     if(this.elem) {
       this.elem.style.width = this.width + "px";
       this.elem.style.height = this.height + "px";
+      this.elem.classList.add(this.class);
       where.appendChild(this.elem);
     }
   }
@@ -29,8 +36,8 @@ let Widget = {
 
 let Button = Object.create(Widget);
 
-Button.setup = function(width, height, label="Default") {
-  this.init(width, height);
+Button.setup = function(buttonClass, width, height, label="Default") {
+  this.init(buttonClass, width, height);
   this.elem = document.createElement("button");
   this.elem.textContent = label;
 };
@@ -39,12 +46,58 @@ Button.build = function(where) {
   this.insert(where);
 };
 
-ready( function() {
-  let onePlayerBtn = Object.create(Button);
-  onePlayerBtn.setup( 125, 30, "One Player");
-  onePlayerBtn.build(moduleContent);
+let onePlayer = Object.create(Button);
+onePlayer.setup( "one-player-btn", 145, 40, "One Player");
+onePlayer.build(modalContent);
+const onePlayerBtn = document.querySelector(".one-player-btn");
 
-  let twoPlayersBtn = Object.create(Button);
-  twoPlayersBtn.setup( 125, 30, "Two Players");
-  twoPlayersBtn.build(moduleContent);
+let twoPlayers = Object.create(Button);
+twoPlayers.setup( "two-players-btn", 145, 40, "Two Players");
+twoPlayers.build(modalContent);
+const twoPlayersBtn = document.querySelector(".two-players-btn");
+
+let again = Object.create(Button);
+again.setup( "again-btn", 100, 40, "Again");
+again.build(wrapper);
+const againBtn = document.querySelector(".again-btn");
+
+setTimeout(openModal, 500);
+
+// modal window
+function openModal() {
+    modalWindow.style.width = "100%";
+}
+
+function closeModal() {
+    modalWindow.style.width = "0%";
+}
+
+twoPlayersBtn.onclick = function() {
+  closeModal();
+  modalContent.style.display = "none";
+};
+
+// two players logic
+fieldContainer.addEventListener("mouseup", function(ev) {
+  // let's make arrays of classes to manipulate them
+  let fieldClassList = Array.from(fieldContainer.classList);
+  let targetClassList = Array.from(ev.target.classList);
+
+  // if not empty display proper svg token
+  if ( fieldClassList.includes("xIsNext") && !targetClassList.includes("notEmpty") ) {
+    ev.target.querySelector(".X").style.display = "block";
+    fieldContainer.classList.remove("xIsNext");
+    fieldContainer.classList.add("oIsNext");
+    ev.target.classList.add("notEmpty");
+  } else if ( fieldClassList.includes("oIsNext") && !targetClassList.includes("notEmpty") ) {
+    ev.target.querySelector(".O").style.display = "block";
+    fieldContainer.classList.remove("oIsNext");
+    fieldContainer.classList.add("xIsNext");
+    ev.target.classList.add("notEmpty");
+  }
 });
+
+// again
+againBtn.onclick = function() {
+  location.reload();
+};
